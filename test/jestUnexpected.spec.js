5931661a6a217fc5569fbbf407c861c4ebef59e4
Alex J Burke <alex@alexjeffburke.com>
2017-08-02 18:30:07 +0200

    Initial commit.

---
+++ test/jestUnexpected.spec.js
@@ -0,0 +1,17 @@
+const expect = require('../lib/jestUnexpected');
+const unexpected = require('unexpected');
+
+describe('jest-unexpected', function() {
+    describe('toEqual()', () => {
+        it('should compare strings failing', () => {
+            unexpected(
+                () => expect('foo').toEqual('bar'),
+                'to throw',
+                "expected 'foo' to equal 'bar'\n" + '\n' + '-foo\n' + '+bar'
+            );
+        });
+        it('should compare strings', () => {
+            unexpected(() => expect('foo').toEqual('foo'), 'not to throw');
+        });
+    });
+});
