7b75a8e2add48c8ee6451df72f6ddcfe5d6d2f7e
Alex J Burke <alex@alexjeffburke.com>
2017-08-02 18:51:40 +0200

    Remove nesting level in tests.

--- test/jestUnexpected.spec.js
+++ test/jestUnexpected.spec.js
@@ -1,67 +1,62 @@
 const expect = require('../lib/jestUnexpected');
 const unexpected = require('unexpected');
 
-describe('jest-unexpected', () => {
-    describe('toBe()', () => {
+describe('toBe()', () => {
+    it('should compare instances failing', () => {
+        unexpected(
+            () => expect({}).toBe({}),
+            'to throw',
+            'expected {} to be {}'
+        );
+    });
+
+    it('should compare instances', () => {
+        const instance = {};
+
+        unexpected(() => expect(instance).toBe(instance), 'not to throw');
+    });
+
+    describe('.not', () => {
         it('should compare instances failing', () => {
+            const instance = {};
+
             unexpected(
-                () => expect({}).toBe({}),
+                () => expect(instance).not.toBe(instance),
                 'to throw',
-                'expected {} to be {}'
+                'expected {} not to be {}'
             );
         });
 
         it('should compare instances', () => {
-            const instance = {};
-
-            unexpected(() => expect(instance).toBe(instance), 'not to throw');
-        });
-
-        describe('.not', () => {
-            it('should compare instances failing', () => {
-                const instance = {};
-
-                unexpected(
-                    () => expect(instance).not.toBe(instance),
-                    'to throw',
-                    'expected {} not to be {}'
-                );
-            });
-
-            it('should compare instances', () => {
-                unexpected(() => expect({}).not.toBe({}), 'not to throw');
-            });
+            unexpected(() => expect({}).not.toBe({}), 'not to throw');
         });
     });
+});
 
-    describe('toEqual()', () => {
+describe('toEqual()', () => {
+    it('should compare strings failing', () => {
+        unexpected(
+            () => expect('foo').toEqual('bar'),
+            'to throw',
+            "expected 'foo' to equal 'bar'\n" + '\n' + '-foo\n' + '+bar'
+        );
+    });
+
+    it('should compare strings', () => {
+        unexpected(() => expect('foo').toEqual('foo'), 'not to throw');
+    });
+
+    describe('.not', () => {
         it('should compare strings failing', () => {
             unexpected(
-                () => expect('foo').toEqual('bar'),
+                () => expect('foo').not.toEqual('foo'),
                 'to throw',
-                "expected 'foo' to equal 'bar'\n" + '\n' + '-foo\n' + '+bar'
+                "expected 'foo' not to equal 'foo'"
             );
         });
 
         it('should compare strings', () => {
-            unexpected(() => expect('foo').toEqual('foo'), 'not to throw');
-        });
-
-        describe('.not', () => {
-            it('should compare strings failing', () => {
-                unexpected(
-                    () => expect('foo').not.toEqual('foo'),
-                    'to throw',
-                    "expected 'foo' not to equal 'foo'"
-                );
-            });
-
-            it('should compare strings', () => {
-                unexpected(
-                    () => expect('foo').not.toEqual('bar'),
-                    'not to throw'
-                );
-            });
+            unexpected(() => expect('foo').not.toEqual('bar'), 'not to throw');
         });
     });
 });
