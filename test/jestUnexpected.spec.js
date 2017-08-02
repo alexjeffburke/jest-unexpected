272c1efa8c1877d48ffdf441c0b0dbc7939ec866
Alex J Burke <alex@alexjeffburke.com>
2017-08-02 18:50:05 +0200

    Implement toBe() assertions.

--- test/jestUnexpected.spec.js
+++ test/jestUnexpected.spec.js
@@ -2,6 +2,38 @@ const expect = require('../lib/jestUnexpected');
 const unexpected = require('unexpected');
 
 describe('jest-unexpected', () => {
+    describe('toBe()', () => {
+        it('should compare instances failing', () => {
+            unexpected(
+                () => expect({}).toBe({}),
+                'to throw',
+                'expected {} to be {}'
+            );
+        });
+
+        it('should compare instances', () => {
+            const instance = {};
+
+            unexpected(() => expect(instance).toBe(instance), 'not to throw');
+        });
+
+        describe('.not', () => {
+            it('should compare instances failing', () => {
+                const instance = {};
+
+                unexpected(
+                    () => expect(instance).not.toBe(instance),
+                    'to throw',
+                    'expected {} not to be {}'
+                );
+            });
+
+            it('should compare instances', () => {
+                unexpected(() => expect({}).not.toBe({}), 'not to throw');
+            });
+        });
+    });
+
     describe('toEqual()', () => {
         it('should compare strings failing', () => {
             unexpected(
@@ -14,10 +46,8 @@ describe('jest-unexpected', () => {
         it('should compare strings', () => {
             unexpected(() => expect('foo').toEqual('foo'), 'not to throw');
         });
-    });
 
-    describe('.not', () => {
-        describe('toEqual()', () => {
+        describe('.not', () => {
             it('should compare strings failing', () => {
                 unexpected(
                     () => expect('foo').not.toEqual('foo'),
