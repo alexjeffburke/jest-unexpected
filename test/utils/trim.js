d4871c4772548ee71fbc1f561159639e0dd90a79
Gustav Nikolaj <gustavnikolaj@gmail.com>
2017-08-12 13:44:08 +0200

    Add a trim method and use template strings for output. (#7)

---
+++ test/utils/trim.js
@@ -0,0 +1,18 @@
+const zipArgs = (strs, args) =>
+    strs.map((str, i) => (i < args.length ? str + args[i] : str)).join('');
+
+const trim = (strs, ...args) => {
+    const str = zipArgs(strs, args);
+
+    const firstIndentMatch = str.match(/^\n(\s+)/);
+    const indent = firstIndentMatch !== null ? firstIndentMatch[1] : null;
+
+    return str
+        .split('\n')
+        .map(str => str.replace(indent, ''))
+        .join('\n')
+        .replace(/^\n/, '')
+        .replace(/\n\s*$/, '');
+};
+
+module.exports = trim;
