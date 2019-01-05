4760a47b41444d8c09527d8e9b486763d642ce3b
Alex J Burke <alex@alexjeffburke.com>
2019-01-05 16:54:43 +0100

    Add UMD build.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
+const pkg = require('./package.json');
+
 const plugins = [
     require('rollup-plugin-commonjs')(),
     require('rollup-plugin-node-resolve')(),
-    require('rollup-plugin-node-globals')(),
-    require('rollup-plugin-babel')({ runtimeHelpers: true })
+    require('rollup-plugin-node-globals')()
 ];
 
 module.exports = [
@@ -10,11 +11,37 @@ module.exports = [
         input: 'src/jestUnexpected.js',
         external: ['unexpected', 'unexpected-sinon', 'sinon'],
         output: {
-            file: 'lib/jestUnexpected.js',
+            file: pkg.main,
             format: 'cjs',
             sourcemap: true,
             strict: false
         },
-        plugins
+        plugins: [
+            ...plugins,
+            require('rollup-plugin-babel')({ runtimeHelpers: true })
+        ]
+    },
+    {
+        input: 'src/jestUnexpected.js',
+        external: ['unexpected', 'unexpected-sinon', 'sinon'],
+        output: {
+            file: pkg.browser,
+            name: 'jestUnexpected',
+            format: 'umd',
+            sourcemap: true,
+            strict: false,
+            globals: {
+                unexpected: 'weknowhow.expect',
+                'unexpected-sinon': 'weknowhow.unexpectedSinon',
+                sinon: 'sinon'
+            }
+        },
+        plugins: [
+            ...plugins,
+            require('rollup-plugin-babel')({
+                plugins: ['external-helpers'],
+                runtimeHelpers: true
+            })
+        ]
     }
 ];
