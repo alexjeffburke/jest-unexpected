6067340adee1703a880d734a0a84a08f5f6d16b6
Alex J Burke <alex@alexjeffburke.com>
2023-04-29 18:43:27 +0200

    Refresh lint.
    Upgrade eslint et al and switch to style checking via pure prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 const config = {
-    extends: ['pretty-standard'],
+    extends: ['standard', 'prettier'],
+    plugins: ['import', 'mocha', 'promise'],
+    overrides: [
+        {
+            files: ['*.spec.js'],
+            env: {
+                mocha: true,
+            },
+            rules: {
+                'no-new': 'off',
+                'mocha/no-exclusive-tests': 'error',
+                'mocha/no-nested-tests': 'error',
+                'mocha/no-identical-title': 'error',
+            },
+        },
+    ],
 };
-
-if (process.stdin.isTTY) {
-    // Enable plugin-prettier when running in a terminal. Allows us to have
-    // eslint verify prettier formatting, while not being bothered by it in our
-    // editors.
-    config.plugins = config.plugins || [];
-    config.plugins.push('prettier');
-    config.rules = config.rules || {};
-    config.rules['prettier/prettier'] = 'error';
-}
 
 module.exports = config;
